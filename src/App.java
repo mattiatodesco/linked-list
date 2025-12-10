b05ed532b41c9c660421db41a945ca0566da8aa7
mattiatodesco <mattia.todesco@marconirovereto.it>
2025-12-10 23:11:23 +0100

    Feat: Implement linked list functionality with append and print methods

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Node n1 = new Node(322);
+        Node n2 = new Node(23);
+        Node n3 = new Node(54);
+        Node n4 = new Node(65);
+        Node n5 = new Node(90);
+
+        CustomList list = new CustomList(null);
+
+        list.append(n1);
+        list.append(n2);
+        list.append(n3);
+        list.append(n4);
+        list.append(n5);
+
+        list.print();
     }
 }
--- src/CustomList.java
+++ src/CustomList.java
@@ -15,13 +15,40 @@ public class CustomList {
      * @param newNode new node to be added
      */
     public void append(Node newNode) {
+        if (head == null)
+            head = newNode;
+        else 
+            appendNext(head, newNode);
 
+    }
+
+    private void appendNext(Node node, Node nuovo){
+        if (node.getNext() == null){
+            node.setNext(nuovo);
+        } else {
+            appendNext(node.getNext(), nuovo);
+        }
     }
 
     /**
      * Outputs every list item on the console
      */
     public void print() {
+        System.out.print("List: [");
+        printNext(head);
 
+    }
+
+    private void printNext(Node node){
+        if(node == null){
+            System.out.println("]");
+            return;
+        } else if (node.getNext() == null){
+            System.out.print(node.getValue());
+        } else {
+            System.out.print(node.getValue() + ", ");
+        }
+
+        printNext(node.getNext());
     }
 }
